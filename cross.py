7e1b15343f378cc3425408bac0192fc44ccc275a
Zeephty <t.flogi.ho@gmail.com>
2024-12-07 16:02:21 +0700

    commit

--- cross.py
+++ cross.py
@@ -3,19 +3,15 @@ import pygame
 if __name__ == '__main__':
     pygame.init()
 
-    s = input("Напишите размер окна в формате (w * h)\n Как пример {300 300}: ").split()
-    if len(s) >= 2 and s[0].isdigit() and s[1].isdigit():
-        size = (int(s[0]), int(s[1]))
+    size = (300, 300)
 
-        screen = pygame.display.set_mode(size)
-        pygame.display.set_caption("Крест")
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption("Крест")
 
-        pygame.draw.line(screen, (255, 255, 255), (0, 0), (size[0], size[1]), 5)
-        pygame.draw.line(screen, (255, 255, 255), (0, size[1]), (size[0], 0), 5)
+    pygame.draw.line(screen, (255, 255, 255), (0, 0), (size[0], size[1]), 5)
+    pygame.draw.line(screen, (255, 255, 255), (0, size[1]), (size[0], 0), 5)
 
-        pygame.display.flip()
-        while pygame.event.wait().type != pygame.QUIT:
-            pass
-        pygame.quit()
-    else:
-        print("Неправильный формат ввода")
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
